50bf9d8c3ac80eb8f915701519f11050f1f0490e
jasonwoolford <jason.woolford115@gmail.com>
2022-09-24 19:02:25 -0400

    Saving before restarting the VM When I come back, I am going to update the play function to compare user input to the randomzied computer input.

--- rps.js
+++ rps.js
@@ -1,28 +1,32 @@
-function getComputerChoice(choice) {
-    choice = Math.floor(Math.random() * 3) + 1;
-
-    switch (choice) {
-        case 1:
-            return "Rock!";
-        case 2:
-            return "Paper!";
-        case 3:
-            return "Scissors!";
-    }
-}
-
 function play(playerSelection,computerSelection) {
-    computerSelection = getComputerChoice();
     let playChoice = window.prompt("Enter your move below");
     playerSelection = playChoice;
-    
-    if (playChoice.toLowerCase() === "rock") {
-        return "You play rock";
-    } else if (playChoice.toLowerCase() === "paper") {
+    let compChoice = getComputerChoice();
+    computerSelection = compChoice;
+
+    if (playChoice.toLowerCase() === "rock" && compChoice === 1) {
+        return "You play rock, computer plays rock, it's a tie!";
+    } else if (playChoice.toLowerCase() === "rock" && compChoice === 2) {
+        return "You play rock, computer plays"
+    }
+     else if (playChoice.toLowerCase() === "paper") {
         return "You play paper";
     } else if (playChoice.toLowerCase() === "scissors") {
         return "You play scissors";
     } else {
         return "What?";
     }
+    }
+
+    function getComputerChoice(choice) {
+        choice = Math.floor(Math.random() * 3) + 1;
+    
+        switch (choice) {
+            case 1:
+                return "Rock!";
+            case 2:
+                return "Paper!";
+            case 3:
+                return "Scissors!";
+        }
     }
