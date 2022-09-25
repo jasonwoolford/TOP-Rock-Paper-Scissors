db9afbe747ed38f320bf2db65368d4084988a8ef
jasonwoolford <jason.woolford115@gmail.com>
2022-09-24 21:28:00 -0400

    Five-set game function built, project technically completed The newly created game() function runs the play() function five times, keeps score of the players, and compares the scores at the end to declare a final winner.  Project is now technically complete!  Any modifications made in the future will be to create a GUI on the webpage for the user to interact with.

--- rps.js
+++ rps.js
@@ -8,21 +8,27 @@ function play(playerSelection,computerSelection) {
     if (playChoice.toLowerCase() === "rock" && computerSelection === 1) {
         return "You play rock, computer plays rock, it's a tie!";
     } else if (playChoice.toLowerCase() === "rock" && computerSelection === 2) {
-        return `You play rock, computer plays paper. Computer wins!`
+        computerScore++;
+        return `You play rock, computer plays paper. Computer wins!`;
     } else if (playChoice.toLowerCase() === "rock" && computerSelection === 3) {
-        return "You play rock, computer plays scissors. You win!"
+        playerScore++;
+        return "You play rock, computer plays scissors. You win!";
     } else if (playChoice.toLowerCase() === "paper" && computerSelection === 1) {
+        playerScore++;
         return "You play paper, computer plays rock.  You win!";
     } else if (playChoice.toLowerCase() === "paper" && computerSelection === 2) {
-        return "You play paper, computer plays paper, it's a tie!"
+        return "You play paper, computer plays paper, it's a tie!";
     } else if (playChoice.toLowerCase() === "paper" && computerSelection === 3) {
-        return "You play paper, computer plays scissors. Computer wins!"
+        computerScore++;
+        return "You play paper, computer plays scissors. Computer wins!";
     } else if (playChoice.toLowerCase() === "scissors" && computerSelection === 1) {
+        computerScore++;
         return "You play scissors, computer plays rock. Computer wins!";
     } else if (playChoice.toLowerCase() === "scissors" && computerSelection === 2) {
-        return "You play scissors, computer plays paper. You win!"
+        playerScore++;
+        return "You play scissors, computer plays paper. You win!";
     } else if (playChoice.toLowerCase() === "scissors" && computerSelection === 3) {
-        return "You play scissors, computer plays scissors, it's a tie!"
+        return "You play scissors, computer plays scissors, it's a tie!";
     } else {
         return "What?";
     }
@@ -39,4 +45,24 @@ function play(playerSelection,computerSelection) {
             case 3:
                 return 3;  //Scissors value
         }
+    }
+
+    function game() { //This function runs the play function five times, keeps score of the players, and declares a final winner after the final loop.
+        playerScore = 0;
+        computerScore = 0;
+        for (let i = 0; i < 5; i++) {
+            play();
+        }
+
+        if (playerScore > computerScore) {
+            return "The player scored " + playerScore + " and the computer scored " + computerScore + "."
+            + " The player wins!";
+        } else if (playerScore < computerScore) {
+            return "The player scored " + playerScore + " and the computer scored " + computerScore + "."
+            + " The computer wins!"
+        } else {
+            return "The player scored " + playerScore + " and the computer score " + computerScore + "."
+            + " It's a tie!  Play again!"
+        }
+
     }
