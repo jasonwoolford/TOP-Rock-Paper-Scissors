9ea42eac10889fe928939f3afc7bd4b2ce0b6b44
jasonwoolford <jason.woolford115@gmail.com>
2022-10-10 07:31:35 -0400

    Added eventListeners to buttons

--- rps.js
+++ rps.js
@@ -24,55 +24,59 @@ section2.appendChild(rockButton);
 section2.appendChild(paperButton);
 section2.appendChild(scissorsButton);
 
-rockButton.addEventListener('click', function play(rock,){});
+
+
+function getComputerChoice(choice) {  //Computer logic for selecting a move
+    choice = Math.floor(Math.random() * 3) + 1;
+
+    switch (choice) {
+        case 1:
+            return 1;  //Rock value
+        case 2:
+            return 2;  //Paper value
+        case 3:
+            return 3;  //Scissors value
+    }
+    console.log(choice);
+}
 
 function play(playerSelection,computerSelection) {
-    getComputerChoice();  //Step one: get input from the computer
+    //getComputerChoice();  //Step one: get input from the computer
     computerSelection = getComputerChoice(); //Step two: assign computer input as arg
-    let playChoice = window.prompt("Enter your move below"); //Step three: get user input
+    playChoice = playerSelection; //Step three: get user input
     playerSelection = playChoice; //Step four: assign user input as arg
 
+    let playerScore = 0;
+    let computerScore = 0;
+
     //This code block lays out all possible outcomes and a fallback condition for unacceptable inputs
-    if (playChoice.toLowerCase() === "rock" && computerSelection === 1) {
+    if (playChoice === "rock" && computerSelection === 1) {
         return "You play rock, computer plays rock, it's a tie!";
-    } else if (playChoice.toLowerCase() === "rock" && computerSelection === 2) {
+    } else if (playChoice === "rock" && computerSelection === 2) {
         computerScore++;
         return `You play rock, computer plays paper. Computer wins!`;
-    } else if (playChoice.toLowerCase() === "rock" && computerSelection === 3) {
+    } else if (playChoice === "rock" && computerSelection === 3) {
         playerScore++;
         return "You play rock, computer plays scissors. You win!";
-    } else if (playChoice.toLowerCase() === "paper" && computerSelection === 1) {
+    } else if (playChoice === "paper" && computerSelection === 1) {
         playerScore++;
         return "You play paper, computer plays rock.  You win!";
-    } else if (playChoice.toLowerCase() === "paper" && computerSelection === 2) {
+    } else if (playChoice === "paper" && computerSelection === 2) {
         return "You play paper, computer plays paper, it's a tie!";
-    } else if (playChoice.toLowerCase() === "paper" && computerSelection === 3) {
+    } else if (playChoice === "paper" && computerSelection === 3) {
         computerScore++;
         return "You play paper, computer plays scissors. Computer wins!";
-    } else if (playChoice.toLowerCase() === "scissors" && computerSelection === 1) {
+    } else if (playChoice === "scissors" && computerSelection === 1) {
         computerScore++;
         return "You play scissors, computer plays rock. Computer wins!";
-    } else if (playChoice.toLowerCase() === "scissors" && computerSelection === 2) {
+    } else if (playChoice === "scissors" && computerSelection === 2) {
         playerScore++;
         return "You play scissors, computer plays paper. You win!";
-    } else if (playChoice.toLowerCase() === "scissors" && computerSelection === 3) {
+    } else if (playChoice === "scissors" && computerSelection === 3) {
         return "You play scissors, computer plays scissors, it's a tie!";
     } else {
         return "What?";
     }
-    }
-
-    function getComputerChoice(choice) {  //Computer logic for selecting a move
-        choice = Math.floor(Math.random() * 3) + 1;
-    
-        switch (choice) {
-            case 1:
-                return 1;  //Rock value
-            case 2:
-                return 2;  //Paper value
-            case 3:
-                return 3;  //Scissors value
-        }
     }
 
     /*
@@ -96,3 +100,15 @@ function play(playerSelection,computerSelection) {
 
     }
     */
+
+    rockButton.addEventListener('click', function() {
+        console.log(play("rock"));
+    });
+    
+    paperButton.addEventListener('click', function(){
+        console.log(play("paper"));
+    });
+
+    scissorsButton.addEventListener('click', function(){
+        console.log(play("scissors"));
+    });
