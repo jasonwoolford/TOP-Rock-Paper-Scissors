a592ebdc6549732c66832fcababe7acd2342238e
jasonwoolford <jason.woolford115@gmail.com>
2022-10-12 08:58:14 -0400

    Scores are kepttrack in two divs Will clean up in a few minutes so that the running score is kept in section 4 alone.

--- rps.js
+++ rps.js
@@ -26,7 +26,7 @@ section2.appendChild(scissorsButton);
 
 const section3 = document.createElement('div');
 section3.classList.add('section3');
-section3.textContent = "Results here"
+section3.textContent = "Game log"
 section3.style.backgroundColor = 'pink'
 mainContainer.appendChild(section3);
 
@@ -34,7 +34,13 @@ const sec3list = document.createElement('ul');
 sec3list.classList.add('sec3p');
 section3.appendChild(sec3list);
 
+const section4 = document.createElement('div');
+section4.classList.add('section4');
+mainContainer.appendChild(section4);
 
+//Global Values
+playerScore = 0;
+computerScore = 0;
 
 function getComputerChoice(choice) {  //Computer logic for selecting a move
     choice = Math.floor(Math.random() * 3) + 1;
@@ -53,40 +59,55 @@ function getComputerChoice(choice) {  //Computer logic for selecting a move
 function play(playerSelection,computerSelection) {
     computerSelection = getComputerChoice(); //Step one: assign computer input as arg
     playChoice = playerSelection; //Step two: get user input
-
-    let playerScore = 0;
-    let computerScore = 0;
-
+    
     //This code block lays out all possible outcomes and a fallback condition for unacceptable inputs
     if (playChoice === "rock" && computerSelection === 1) {
-        return "You play rock, computer plays rock, it's a tie!";
+        return "You play rock, computer plays rock, it's a tie!" + "/ / /"
+        + "Current player score: " + playerScore + " Current computer score: " + computerScore;
     } else if (playChoice === "rock" && computerSelection === 2) {
         computerScore++;
-        return `You play rock, computer plays paper. Computer wins!`;
+        return `You play rock, computer plays paper. Computer wins!` + "/ / /"
+        + "Current player score: " + playerScore + " Current computer score: " + computerScore;;
     } else if (playChoice === "rock" && computerSelection === 3) {
         playerScore++;
-        return "You play rock, computer plays scissors. You win!";
+        return "You play rock, computer plays scissors. You win!" + "/ / /"
+        + "Current player score: " + playerScore + " Current computer score: " + computerScore;;
     } else if (playChoice === "paper" && computerSelection === 1) {
         playerScore++;
-        return "You play paper, computer plays rock.  You win!";
+        return "You play paper, computer plays rock.  You win!" + "/ / /"
+        + "Current player score: " + playerScore + " Current computer score: " + computerScore;;
     } else if (playChoice === "paper" && computerSelection === 2) {
-        return "You play paper, computer plays paper, it's a tie!";
+        return "You play paper, computer plays paper, it's a tie!" + "/ / /"
+        + "Current player score: " + playerScore + " Current computer score: " + computerScore;;
     } else if (playChoice === "paper" && computerSelection === 3) {
         computerScore++;
-        return "You play paper, computer plays scissors. Computer wins!";
+        return "You play paper, computer plays scissors. Computer wins!" + "/ / /"
+        + "Current player score: " + playerScore + " Current computer score: " + computerScore;;
     } else if (playChoice === "scissors" && computerSelection === 1) {
         computerScore++;
-        return "You play scissors, computer plays rock. Computer wins!";
+        return "You play scissors, computer plays rock. Computer wins!" + "/ / /"
+        + "Current player score: " + playerScore + " Current computer score: " + computerScore;;
     } else if (playChoice === "scissors" && computerSelection === 2) {
         playerScore++;
-        return "You play scissors, computer plays paper. You win!";
+        return "You play scissors, computer plays paper. You win!" + "/ / /"
+        + "Current player score: " + playerScore + " Current computer score: " + computerScore;;
     } else if (playChoice === "scissors" && computerSelection === 3) {
-        return "You play scissors, computer plays scissors, it's a tie!";
+        return "You play scissors, computer plays scissors, it's a tie!" + "/ / /"
+        + "Current player score: " + playerScore + " Current computer score: " + computerScore;;
     } else {
         return "What?";
     }
     }
 
+function compare() {
+    if (playerScore === 5) {
+        return "Player reached five wins!  Player wins!"
+    } else if (computerScore === 5) {
+        return "Computer reached five wins! Computer wins!"
+    } else {
+        return "Current player score is " + playerScore + " and the current computer score is " + computerScore;
+    }
+}
     function game() { //This function runs the play function five times, keeps score of the players, and declares a final winner after the final loop.
         playerScore = 0;
         computerScore = 0;
@@ -101,26 +122,35 @@ function play(playerSelection,computerSelection) {
             return "The player scored " + playerScore + " and the computer scored " + computerScore + "."
             + " The computer wins!";
         } else {
-            return "The player scored " + playerScore + " and the computer score " + computerScore + "."
+            return "The player scored " + playerScore + " and the computer scored " + computerScore + "."
             + " It's a tie!  Play again!";
         }
 
     }
 
-    rockButton.addEventListener('click', function() {
-        let result = "";
-        result = play("rock");
-        sec3list.textContent = result;
-    });
+rockButton.addEventListener('click', function() {
+    let result = "";
+    result = play("rock");
+    sec3list.textContent = result;
+    let final = "";
+    final = compare();
+    section4.textContent = final;    
+});
     
-    paperButton.addEventListener('click', function(){
-        let result = "";
-        result = play("paper");
-        sec3list.textContent = result;
-    });
+paperButton.addEventListener('click', function(){
+    let result = "";
+    result = play("paper");
+    sec3list.textContent = result;
+    let final = "";
+    final = compare();
+    section4.textContent = final; 
+});
 
-    scissorsButton.addEventListener('click', function(){
-        let result = "";
-        result = play("scissors");
-        sec3list.textContent = result;
-    });
+scissorsButton.addEventListener('click', function(){
+    let result = "";
+    result = play("scissors");
+    sec3list.textContent = result;
+    let final = "";
+    final = compare();
+    section4.textContent = final; 
+});
